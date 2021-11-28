fb57a215467e3c4c6792c1cb27f4c75728acd0c2
Daniela Costa <dccosta@neadvance.com>
2021-11-28 19:54:55 +0000

    Tela quero ajudar; Get instituicoes;

--- src/app/admin/listar-instituicao/listar-instituicao.component.ts
+++ src/app/admin/listar-instituicao/listar-instituicao.component.ts
@@ -1,7 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { BaseComponent } from '../base.component';
+import  axios  from "axios";
 
+export class Filtro {
+  nome: string;
+  cnpj: string;
+  cidade: string;
+}
 @Component({
   selector: 'app-listar-instituicao',
   templateUrl: './listar-instituicao.component.html',
@@ -9,16 +15,32 @@ import { BaseComponent } from '../base.component';
 })
 export class ListarInstituicaoComponent extends BaseComponent implements OnInit {
 
-  instituticoes: any[] = [];
+  filtro: Filtro = new Filtro();
+  instituicoes: any[] = [];
 
   constructor(private router: Router) {
     super();
   }
 
   ngOnInit(): void {
+    this.buscarInstituicoes();
   }
 
   novaInstituicao() {
     this.router.navigate(['manter-instituicao']);
+  }
+
+  async buscarInstituicoes(){
+    const inst = await axios.get('https://franco-amor-api.herokuapp.com/instituicoes');
+
+    inst.data.forEach(element => {
+      let inst = { nome: element.nome,
+                  email: element.email,
+                  cidade: element.endereco.cidade,
+                  cnpj: element.cnpj,
+                  telefone: element.telefone
+      }
+      this.instituicoes.push(inst);
+    });
   }
 }
